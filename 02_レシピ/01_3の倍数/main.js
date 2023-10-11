2a13b102e9b5150918b55f0d26956e03311ed1fe
iwahashi0910 <i.yuya0910@gmail.com>
2023-10-11 22:49:09 +0900

    ★★ - ３が大好きまでを学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,8 +1,18 @@
+const judge3 = function (n) {
+  while (n) {
+    if (n % 10 === 3) {
+      return true
+    }
+    n = Math.floor(n / 10)
+  }
+  return false
+}
+
 // 引数 number を受け取る関数
 const genkiFunction = function (number) {
   // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
+    if (n % 3 === 0 || judge3(n)) {
       console.log(n + "!!!!!!!")
     } else {
       console.log(n)
@@ -11,3 +21,19 @@ const genkiFunction = function (number) {
 }
 
 genkiFunction(100)
+
+const FizzBuzz = function (number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0 && n % 5 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 3 === 0) {
+      console.log("Fizz")
+    } else if (n % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(n)
+    }
+  }
+}
+
+FizzBuzz(100)
